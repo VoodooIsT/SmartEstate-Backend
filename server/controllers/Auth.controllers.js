e85cc519bdad07a0302f374e09e65d32c7e3bd0d
VoodooIsT <abshukla773p@gmail.com>
2023-10-10 10:46:13 +0530

    created signin API

--- server/controllers/Auth.controllers.js
+++ server/controllers/Auth.controllers.js
@@ -1,5 +1,8 @@
  import User from "../models/User.js"
  import bcrypt from "bcrypt"
+ import errorHandler from "../utils/error.js"
+ import jwt from 'jsonwebtoken'
+
  
  export const signup = async (req, res, next) => {
     const { userName, email, password } = req.body;
@@ -14,4 +17,19 @@
         next(err);
     }
  };
+
+ export const signin = async(req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const validUser = await User.find({ email });
+        if(!validUser) return next(errorHandler(404, "User not found."));
+        const validPassword = bcrypt.compareSync(password,validUser.password);
+        if(!validPassword) return next(errorHandler(401, "Wrong Credentials"));
+        const token = jwt.sign( { id:validUser._id}, process.env.JWT_SECRET);
+        res.cockies('access_token', token, { httpOnly: true }).status(200).json(validUser);
+    } catch(error) {
+        next(error);
+    }
+ }
  
