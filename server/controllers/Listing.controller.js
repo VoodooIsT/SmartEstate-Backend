614b0b66c72a87761def3acd1dd64ee5848b4db9
VoodooIsT <abshukla773p@gmail.com>
2023-10-22 11:18:36 +0530

    created Listing schema

--- server/controllers/Listing.controller.js
+++ server/controllers/Listing.controller.js
@@ -1,5 +1,13 @@
-export const userListing = async(req, res, next) => {
-    
+import Listing from "../models/Listing.js"
+
+export const createListing = async(req, res, next) => {
+    try{
+
+        const listing = await Listing.create(req.body);
+
+    } catch(err) {
+
+    }
 }
 export const getUSer = async(req, res, next) => {
     
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.js";
 import authRouter from "./routes/auth.js";
 import cookieParser from "cookie-parser";
+import listingRouter from './routes/listing.js'
 
 
 dotenv.config();
@@ -27,6 +28,7 @@ app.use(cookieParser());
 
 app.use('/api/v1', userRouter);
 app.use('/api/v1', authRouter);
+app.use('/api/v1', listingRouter)
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
