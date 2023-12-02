bc56b0c7df0e15d05211150a50f27cb8cb2fbf4b
VoodooIsT <abshukla773p@gmail.com>
2023-12-02 20:34:08 +0530

    Create get user listing API

--- server/controllers/User.controller.js
+++ server/controllers/User.controller.js
@@ -1,17 +1,17 @@
-import bcrypt from 'bcrypt';
-import User from '../models/User.js';
-import { errorHandler } from '../utils/error.js';
-
+import bcrypt from "bcrypt";
+import User from "../models/User.js";
+import { errorHandler } from "../utils/error.js";
+import Listing from "../models/Listing.js";
 
 export const test = (req, res) => {
   res.json({
-    message: 'Api route is working!',
+    message: "Api route is working!",
   });
 };
 
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
-    return next(errorHandler(401, 'You can only update your own account!'));
+    return next(errorHandler(401, "You can only update your own account!"));
   try {
     if (req.body.password) {
       req.body.password = bcrypt.hashSync(req.body.password, 10);
@@ -40,14 +40,26 @@ export const updateUser = async (req, res, next) => {
 
 export const deleteUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
-    return next(errorHandler(401, 'You can only delete your own account!'));
+    return next(errorHandler(401, "You can only delete your own account!"));
   try {
     await User.findByIdAndDelete(req.params.id);
-    res.clearCookie('access_token');
-    res.status(200).json('User has been deleted!');
+    res.clearCookie("access_token");
+    res.status(200).json("User has been deleted!");
   } catch (error) {
     next(error);
   }
 };
 
-  
+export const getUserListing = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json({
+        success: true,
+        listings,
+      });
+    } catch (err) {}
+  } else {
+    return next(errorHandler(401, "You can only view your own listings!"));
+  }
+};
--- server/routes/listing.js
+++ server/routes/listing.js
@@ -1,11 +1,12 @@
 import  express  from "express";
 
-import { createListing, deleteListing } from "../controllers/Listing.controller.js";
+import { createListing, deleteListing, updateListing } from "../controllers/Listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router()
 
 router.post('/create', verifyToken, createListing);
+router.post('/update/:id', verifyToken, updateListing);
 router.delete('/delete-listing:id', verifyToken, deleteListing)
 
 export default router;
