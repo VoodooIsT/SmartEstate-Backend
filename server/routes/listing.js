8360c33805290b5b43a71ef8aa824dd9782a59db
VoodooIsT <abshukla773p@gmail.com>
2023-12-02 22:11:58 +0530

    Completed getListing API

--- server/routes/listing.js
+++ server/routes/listing.js
@@ -1,12 +1,17 @@
-import  express  from "express";
+import express from "express";
 
-import { createListing, deleteListing, updateListing } from "../controllers/Listing.controller.js";
+import {
+  createListing,
+  deleteListing,
+  getListing,
+  updateListing,
+} from "../controllers/Listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/create', verifyToken, createListing);
-router.post('/update/:id', verifyToken, updateListing);
-router.delete('/delete-listing:id', verifyToken, deleteListing)
-
+router.post("/create", verifyToken, createListing);
+router.post("/updated/:id", verifyToken, updateListing);
+router.delete("/delete-listing/:id", verifyToken, deleteListing);
+router.get("/get/:id", getListing);
 export default router;
